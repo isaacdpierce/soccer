34627c18ae4571b3d91cd238f7ecab0f45ca1083
Isaac Pierce <contact@isaacdpierce.com>
2018-11-19 18:47:51 -0800

    Add logo component to header and footer

--- src/Components/Header_footer/Header.js
+++ src/Components/Header_footer/Header.js
@@ -4,6 +4,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 
 import { Link } from 'react-router-dom';
+import { CityLogo } from '../ui/icons';
 
 class Header extends Component {
   render() {
@@ -19,7 +20,9 @@ class Header extends Component {
       >
         <Toolbar style={{ display: 'flex' }}>
           <div style={{ flexGrow: 1 }}>
-            <div className="header_logo">LOGO</div>
+            <div className="header_logo">
+              <CityLogo link={true} linkTo="/" width="70px" height="70px" />
+            </div>
           </div>
           <Link to="/the_team">
             <Button color="inherit"> The Team</Button>
